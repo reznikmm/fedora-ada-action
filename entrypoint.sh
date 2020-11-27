12ad9311bfe15fe52eb688148f46758b32d90cd7
Maxim Reznik <reznikmm@gmail.com>
2020-11-27 18:14:05 +0200

    Call `spectool` to download missing sources

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ FILE=`grep ^Source0: $SPEC|sed -e 's/^.* //'`
 echo FILE:$FILE
 tar caf ~user/rpmbuild/SOURCES/$FILE --exclude-vcs -C ${GITHUB_WORKSPACE} .
 dnf builddep --assumeyes --spec $SPEC
+do_user spectool -g -R $SPEC
 
 mkdir --mode=777 $GITHUB_WORKSPACE/x86_64
 do_user rpmbuild -bb $SPEC --define "_rpmdir\ ${GITHUB_WORKSPACE}"
